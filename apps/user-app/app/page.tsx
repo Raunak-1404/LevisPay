a789fd4bd50f1d3493c6f3579fb5797fbd5f1fca
Raunak-1404 <jijotiaraunak14@gmail.com>
2025-01-23 19:12:16 +0530

    Configure Recoil across application

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,13 @@
 import { PrismaClient } from "@repo/db/client";
 
 const client = new PrismaClient();
-export default function Home() {
-  return <div className="text-red-400">HEllo Again</div>;
+
+
+export default async function Home() {
+ 
+  return (
+    <div className="bg-red-500">
+      Hello from home page
+    </div>
+  );
 }
