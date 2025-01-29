3aa13426646343ab892da048dc476fbb2f3b27fb
Raunak-1404 <jijotiaraunak14@gmail.com>
2025-01-29 12:27:15 +0530

    fixed indentation

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -9,7 +9,7 @@ export function Card({
 }): JSX.Element {
   return (
     <div className="border p-4 ">
-      <h1 className="text-xl border-b pb-2">{title}</h1>
+      <h1 className="text-xl border-b pb-2 mx-5">{title}</h1>
       <div>{children}</div>
     </div>
   );
