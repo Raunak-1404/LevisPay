0c11816c5278f00b04644425e2a3f52673e9e5fc
Raunak-1404 <jijotiaraunak14@gmail.com>
2025-01-29 12:26:02 +0530

    Added a SendMoney Component

---
+++ apps/user-app/components/SendMoneyCard.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@repo/ui/button";
+import { Card } from "@repo/ui/card";
+import { Center } from "@repo/ui/Center";
+import { TextInput } from "@repo/ui/TextInput";
+import { useState } from "react";
+
+export default function SendMoneyCard() {
+
+    const [number, setNumber] = useState("");
+    const [amount, setAmount] = useState("");
+
+  return (
+    <div className="h-[84vh] ">
+      <Center>
+        <Card title="Send">
+          <div className="min-w-72 pt-2 px-5">
+            <TextInput
+              placeholder={"Number"}
+              label="Number"
+              onChange={(value) => {
+                setNumber(value);
+              }}
+            />
+            <TextInput
+              placeholder={"Amount"}
+              label="Amount"
+              onChange={(value) => {
+                setAmount(value);
+              }}
+            />
+            <div className="pt-4 flex justify-center">
+              <Button onClick={() => {}}>Send</Button>
+            </div>
+          </div>
+        </Card>
+      </Center>
+    </div>
+  );
+}
